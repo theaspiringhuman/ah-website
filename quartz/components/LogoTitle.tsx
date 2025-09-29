cdeac8cb6773b953ab42bdd42c1389513111bc16
theaspiringhuman <iamtheaspiringhuman@gmail.com>
2025-09-29 13:05:49 -0700

    Create LogoTitle.tsx

---
+++ quartz/components/LogoTitle.tsx
@@ -0,0 +1,8 @@
+export default function LogoTitle() {
+  return (
+    <a href="/" class="site-title">
+      <img src="/aspiring-human-logo.png" alt="Aspiring Human" style={{ height: "40px" }} />
+    </a>
+  )
+}
+
