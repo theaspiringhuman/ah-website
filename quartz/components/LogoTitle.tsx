a8c740f2b8d069a2ebad99db79230b156203c31c
theaspiringhuman <iamtheaspiringhuman@gmail.com>
2025-09-29 13:41:12 -0700

    Update LogoTitle.tsx
    Trying again to swap the title text for a logo

--- quartz/components/LogoTitle.tsx
+++ quartz/components/LogoTitle.tsx
@@ -1,8 +1,17 @@
-export default function LogoTitle() {
-  return (
-    <a href="/" class="site-title">
-      <img src="/aspiring-human-logo.png" alt="Aspiring Human" style={{ height: "40px" }} />
-    </a>
-  )
-}
+import { QuartzComponent } from "./cfg";
 
+const LogoTitle: QuartzComponent = () => {
+  return {
+    element: (
+      <a href="/" class="site-title">
+        <img
+          src="/aspiring-human-logo.png"
+          alt="Aspiring Human"
+          style={{ height: "40px" }}
+        />
+      </a>
+    ),
+  };
+};
+
+export default LogoTitle;
