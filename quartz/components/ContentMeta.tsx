f1578c6100b1f7cd18a35cc0433e2036e8478f61
theaspiringhuman <iamtheaspiringhuman@gmail.com>
2025-10-04 15:39:59 -0700

    Update ContentMeta.tsx

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -27,29 +27,30 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
     const text = fileData.text
 
     if (text) {
-      const segments: (string | JSX.Element)[] = []
+  const segments: (string | JSX.Element)[] = []
 
-      if (fileData.dates) {
-        segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
-      }
+  // Remove this block to get rid of dates entirely
+  // if (fileData.dates) {
+  //   segments.push(<Date date={getDate(cfg, fileData)!} locale={cfg.locale} />)
+  // }
 
-      // Display reading time if enabled
-      if (options.showReadingTime) {
-        const { minutes, words: _words } = readingTime(text)
-        const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
-          minutes: Math.ceil(minutes),
-        })
-        segments.push(<span>{displayedTime}</span>)
-      }
+  // Display reading time if enabled
+  if (options.showReadingTime) {
+    const { minutes, words: _words } = readingTime(text)
+    const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
+      minutes: Math.ceil(minutes),
+    })
+    segments.push(<span>{displayedTime}</span>)
+  }
 
-      return (
-        <p show-comma={options.showComma} class={classNames(displayClass, "content-meta")}>
-          {segments}
-        </p>
-      )
-    } else {
-      return null
-    }
+  return (
+    <p show-comma={options.showComma} class={classNames(displayClass, "content-meta")}>
+      {segments}
+    </p>
+  )
+} else {
+  return null
+}
   }
 
   ContentMetadata.css = style
