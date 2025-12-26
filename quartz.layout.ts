666c1997cb64d15cb1165ad6dd960be5af947246
theaspiringhuman <iamtheaspiringhuman@gmail.com>
2025-12-26 12:29:09 -0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,6 +20,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
+    Component.Backlinks(),
     // Component.TagList(), // removed to hide tags
   ],
   left: [
