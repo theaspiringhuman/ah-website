efa4e6c44e52e752ee0638957c8c9ee70b07a31f
theaspiringhuman <iamtheaspiringhuman@gmail.com>
2026-04-02 17:50:57 -0700

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,26 +1,33 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-    const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
-  const year = new Date().getFullYear()
-  const links = opts?.links ?? []
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
 
-  return (
-    <footer class={`${displayClass ?? ""}`}>
-      <div class="footer-inner">
-        <span>Aspiring Human · {year}</span>
-        <img src="/footer.png" alt="Footer" />
-      </div>
-    </footer>
-  )
-}
+    return (
+      <footer class={`${displayClass ?? ""}`}>
+        {links && Object.keys(links).length > 0 && (
+          <ul>
+            {Object.entries(links).map(([text, link]) => (
+              <li key={text}>
+                <a href={link}>{text}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+        <div class="footer-inner">
+          <span>Aspiring Human · {year}</span>
+          <img src="/footer.png" alt="Footer" />
+        </div>
+      </footer>
+    )
+  }
 
   Footer.css = style
   return Footer
